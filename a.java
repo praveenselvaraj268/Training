094729216376ed64969e4c6172a2a8316fe600f1
training_C2D.02.11 <training_C2D.02.11@accenture.com>
2018-08-09 16:13:43 +0530

    commiting the branch

--- a.java
+++ a.java
@@ -1,3 +1,22 @@
 Working in git tool
 
 got error
+
+nothing is clearedgit
+
+
+i can make a change.
+
+git is a distributed version control.
+
+git hub is a git repository manager.
+
+git pull command is used to get the changes from the remote repository to local repository.
+
+git reset is used to undo the commit.
+
+worked on git and installed tortoise git
+
+second change
+
+second branck pushed to branch
